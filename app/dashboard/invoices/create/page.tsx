586f284cd298a1c7a347c27255631b56453ffc03
DiegoFandinoDev <diegofandino.u@gmail.com>
2026-07-29 20:58:36 -0500

    Metada: Created the metadata for all page with title and description, and templates for individual pages

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,6 +1,11 @@
 import { fetchCustomers } from "@/app/lib/data";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import Form from "@/app/ui/invoices/create-form";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'create page'
+}
 
 export default async function CreateInvoicePage() {
     const customers = await fetchCustomers();
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 import { inter } from '@/app/ui/fonts';
 import '@/app/ui/global.css';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Interactive Dashboard',
+    default: 'Interactive Dashboard',
+  },
+  description: 'Interactive dashboard made with nextjs by fandino',
+}
 
 export default function RootLayout({
   children,
